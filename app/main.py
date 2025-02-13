6aef57f734c1a506e2cb03517bd1dd417260607a
erickuce1509 <erickuce1509@gmail.com>
2025-02-13 14:23:50 -0500

    CI/CD para ReadProduct en AWS EC2

---
+++ app/main.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI
+from app.routes import router
+from app.database import engine, Base
+from fastapi.middleware.cors import CORSMiddleware  # ✅ Importar CORSMiddleware
+
+
+# Crear las tablas en la base de datos si no existen
+Base.metadata.create_all(bind=engine)
+
+# Inicializar FastAPI
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  # Permitir solicitudes desde el frontend
+    allow_credentials=True,
+    allow_methods=["*"],  # Permitir todos los métodos (GET, POST, DELETE, etc.)
+    allow_headers=["*"],  # Permitir todos los encabezados
+)
+
+# Incluir las rutas
+app.include_router(router)
+
+@app.get("/")
+def root():
+    return {"message": "ReadProduct API is running!"}
